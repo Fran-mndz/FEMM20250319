2eebb86a2d1178712239a8b23fedeb3c4f9c91c2
Alumno <Alumno@M01-CI>
2025-03-19 08:12:02 -0600

    Agregar archivos de proyecto.

---
+++ FEMM20250319/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FEMM20250319.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
