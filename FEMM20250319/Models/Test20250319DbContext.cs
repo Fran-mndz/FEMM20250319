755755de1b2d345976754335a0309b3d879ad15b
Francisco Medina <shiro54828@gmail.com>
2025-03-19 11:48:29 -0600

    Cambios al login, insercion de productos de prueba, paginacion, falta arreglar el segundo buscador en Productos y ajustar el inicio de sesión

--- FEMM20250319/Models/Test20250319DbContext.cs
+++ FEMM20250319/Models/Test20250319DbContext.cs
@@ -25,7 +25,7 @@ public partial class Test20250319DbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=M17-CI;Initial Catalog=Test20250319DB;User ID=marvin;Password=12345;Encrypt=False");
+        => optionsBuilder.UseSqlServer("Data Source=M07-CII;Initial Catalog=Test20250319DB;Integrated Security=True;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
