33a7b3fe75eef72eb92c7f2994e033b21dd4d8ff
Francisco Medina <shiro54828@gmail.com>
2025-03-19 09:08:22 -0600

    Modificacion de palabras en ingles a español

--- FEMM20250319/Program.cs
+++ FEMM20250319/Program.cs
@@ -1,7 +1,15 @@
+using FEMM20250319.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext <Test20250319DbContext> (options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+
 
 var app = builder.Build();
 
